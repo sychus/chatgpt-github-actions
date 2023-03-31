c67db793f76f0fa59eca92850d58578190660dc6
Hugo <hugo@blackthorn.io>
2023-03-31 17:27:03 -0300

    change name

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 # Automated Code Review using the ChatGPT language model
 
-## Import statements
+# Import statements
 import argparse
 import openai
 import os
 import requests
 from github import Github
 
-## Adding command-line arguments
+# Adding command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--openai_api_key', help='Your OpenAI API Key')
 parser.add_argument('--github_token', help='Your Github Token')
 parser.add_argument('--github_pr_id', help='Your Github PR ID')
-parser.add_argument('--openai_engine', default="text-davinci-002", help='GPT-3 model to use. Options: text-davinci-002, text-babbage-001, text-curie-001, text-ada-001')
-parser.add_argument('--openai_temperature', default=0.5, help='Sampling temperature to use. Higher values means the model will take more risks. Recommended: 0.5')
-parser.add_argument('--openai_max_tokens', default=2048, help='The maximum number of tokens to generate in the completion.')
-parser.add_argument('--mode', default="files", help='PR interpretation form. Options: files, patch')
+parser.add_argument('--openai_engine', default="text-davinci-002",
+                    help='GPT-3 model to use. Options: text-davinci-002, text-babbage-001, text-curie-001, text-ada-001')
+parser.add_argument('--openai_temperature', default=0.5,
+                    help='Sampling temperature to use. Higher values means the model will take more risks. Recommended: 0.5')
+parser.add_argument('--openai_max_tokens', default=2048,
+                    help='The maximum number of tokens to generate in the completion.')
+parser.add_argument('--mode', default="files",
+                    help='PR interpretation form. Options: files, patch')
 args = parser.parse_args()
 
-## Authenticating with the OpenAI API
+# Authenticating with the OpenAI API
 openai.api_key = args.openai_api_key
 
-## Authenticating with the Github API
+# Authenticating with the Github API
 g = Github(args.github_token)
 
 
@@ -29,7 +33,7 @@ def files():
     repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
     pull_request = repo.get_pull(int(args.github_pr_id))
 
-    ## Loop through the commits in the pull request
+    # Loop through the commits in the pull request
     commits = pull_request.get_commits()
     for commit in commits:
         # Getting the modified files in the commit
@@ -37,7 +41,8 @@ def files():
         for file in files:
             # Getting the file name and content
             filename = file.filename
-            content = repo.get_contents(filename, ref=commit.sha).decoded_content
+            content = repo.get_contents(
+                filename, ref=commit.sha).decoded_content
 
             # Sending the code to ChatGPT
             response = openai.Completion.create(
@@ -59,7 +64,8 @@ def patch():
     content = get_content_patch()
 
     if len(content) == 0:
-        pull_request.create_issue_comment(f"Patch file does not contain any changes")
+        pull_request.create_issue_comment(
+            f"Patch file does not contain any changes")
         return
 
     parsed_text = content.split("diff")
@@ -74,7 +80,8 @@ def patch():
 
             response = openai.Completion.create(
                 engine=args.openai_engine,
-                prompt=(f"Summarize what was done in this diff:\n```{diff_text}```"),
+                prompt=(
+                    f"Summarize what was done in this diff:\n```{diff_text}```"),
                 temperature=float(args.openai_temperature),
                 max_tokens=int(args.openai_max_tokens)
             )
@@ -86,7 +93,8 @@ def patch():
         except Exception as e:
             error_message = str(e)
             print(error_message)
-            pull_request.create_issue_comment(f"ChatGPT was unable to process the response about {file_name}")
+            pull_request.create_issue_comment(
+                f"ChatGPT was unable to process the response about {file_name}")
 
 
 def get_content_patch():
